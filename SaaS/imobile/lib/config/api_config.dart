ba1288cadbdb28c94cb34ab0840f0fe1e0d86ce6
Duval <dongmoduval269@gmail.com>
2026-06-27 13:14:22 +0100

    modiffication du port

--- SaaS/imobile/lib/config/api_config.dart
+++ SaaS/imobile/lib/config/api_config.dart
@@ -2,7 +2,8 @@ class ApiConfig {
   // Pour émulateur Android: http://10.0.2.2:8000
   // Pour appareil physique: http://VOTRE_IP:8000
   // Pour iOS simulator: http://localhost:8000
-  static const String baseUrl = 'http://10.0.2.2:8000';
+  // Pour web (localhost): http://127.0.0.1:8000
+  static const String baseUrl = 'http://127.0.0.1:8000';
 
   // API Endpoints
   static const String feed = '/api/immotok/feed';
